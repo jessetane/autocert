9b52a555c12e1c42ee1d8e4d90ead60e3d7ec8d3
Jesse Tane <jesse.tane@gmail.com>
2016-07-11 18:31:20 -0400

    update test

--- test.js
+++ test.js
@@ -1,17 +1,28 @@
-var fs = require('fs')
 var http = require('http')
 var https = require('https')
 var autocert = require('./')
 
+var challenges = {}
+
 http.createServer((req, res) => {
-  fs.createReadStream(__dirname + req.url)
-    .pipe(res)
+  console.log(req.method + ' ' + req.url)
+  console.log('headers:', req.headers)
+
+  var proof = challenges[req.url]
+  console.log('proof:', proof)
+
+  if (proof) {
+    res.end(proof)
+  } else {
+    res.statusCode = 404
+    res.end('not found')
+  }
 }).listen(8080)
 
-https.createServer(autocert({
+https.createServer(autocert.tlsOpts({
+  url: 'https://acme-staging.api.letsencrypt.org',
   email: 'info@example.com',
-  storage: __dirname,
-  challenges: __dirname,
+  challenges,
 }), (req, res) => {
   res.end('secure af')
 }).listen(4430)
